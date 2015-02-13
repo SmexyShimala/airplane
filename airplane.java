35929090e849b1215f252bef4c06b606e3f5d927
Houston Shimala <jshimala@sjccs142.saintjoe.edu>
2015-02-13 15:27:09 -0500

    Airplane#

---
+++ airplane.java
@@ -0,0 +1,15 @@
+public class airplane {	
+	public String make;
+	public String model;
+	public int year;
+	public int capacity;
+
+/*-------------------------------------------*/
+
+	Airplane(String thisMake, String thisModel, int thisYear, int thisCapacity) {
+	make = thisMake;
+	model = thisModel;
+	year = thisYear;
+	capacity = thisCapacity;
+	}
+}
